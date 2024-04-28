c2289ce3491b6077d88a7e1174756ab806505dee
ultraman <1394466835@qq.com>
2024-04-28 21:56:29 +0800

    feat: base layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="app">
+      <header className="header">
+        <h1>Tiny Trello</h1>
+        <img src={reactLogo} className="logo" alt="logo" />
+      </header>
+      <main className="main-board">
+        <section className="column column-todo">
+          <h2>待处理</h2>
+        </section>
+        <section className="column column-ongoing">
+          <h2>进行中</h2>
+        </section>
+        <section className="column column-done">
+          <h2>已完成</h2>
+        </section>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
